7888e4e81dfad43920cad2e7800914b52df10c95
Ruben <55796855+RubiMaistro@users.noreply.github.com>
2023-03-07 19:06:48 +0200

    feat(Project): add backend tasks 2.0
    Week of Days
    Minimum deletion cost avoid repeating letters
    Resolve the riddle

--- BackendTasks/Pages/Index.cshtml.cs
+++ BackendTasks/Pages/Index.cshtml.cs
@@ -90,5 +90,100 @@ namespace BackendTasks.Pages
             data[m] = data[n];
             data[n] = temporary;
         }
+
+        // Week of Days
+
+        private int daysCount = 7;
+        private string[] days = new string[]
+            { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+
+        public string WeekOfDay(string S, int K)
+        {
+            int index = getIndex(S, days);
+
+            int indexForSPlusK = (index + K) % daysCount;
+
+            return days[indexForSPlusK];
+        }
+
+        private int getIndex(string S, string[] days)
+        {
+            for (int i = 0, c = days.Length; i < c; ++i)
+            {
+                if (days[i] == (S))
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        // Minimum deletion cost avoid repeating letters
+        public int GetTotalCost(string S, int[] C)
+        {
+            var characters = S.ToCharArray();
+            var firstLetter = characters[0];
+
+            int totalCost = 0;
+            int maxCost = C[0];
+            int currentTotalCost = C[0];
+
+            for (int i = 1; i < S.Length; i++)
+            {
+                var c = characters[i];
+                int currentCost = C[i];
+                if (c == firstLetter)
+                {
+                    maxCost = maxCost > currentCost ? maxCost : currentCost;
+                    currentTotalCost += currentCost;
+                }
+                else
+                {
+                    totalCost += currentTotalCost - maxCost;
+                    firstLetter = c;
+                    maxCost = currentCost;
+                    currentTotalCost = currentCost;
+                }
+            }
+            totalCost += currentTotalCost - maxCost;
+            return totalCost;
+        }
+
+        // Resolve the riddle
+        public string GetResolveRiddle(string riddle)
+        {
+            string result = "";
+
+            char prev = '\0';
+            for (int i = 0; i < riddle.Length; i++)
+            {
+                char current = riddle[i];
+                if (current == '?')
+                {
+                    char next = '\0';
+                    if (i != riddle.Length - 1)
+                    {
+                        next = riddle[i + 1];
+                    }
+                    current = GetCurrectChar(prev, next);
+                }
+                result += current;
+                prev = current;
+            }
+            return result;
+        }
+
+        private char GetCurrectChar(char prev, char next)
+        {
+            var characters = "abcdefghijklmnopqrstuvz".ToCharArray();
+            var rand = new Random();
+
+            while (true)
+            {
+                var randomIndex = rand.Next(characters.Length);
+                if (prev != characters[randomIndex] && next != characters[randomIndex])
+                    return characters[randomIndex];
+            }
+        }
     }
 }
